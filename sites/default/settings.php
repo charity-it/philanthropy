e62ee7059577d1c2efdd838d7afe5932196912ca
Naomi Guyer <naomi@silverstripe.com>
2013-12-07 09:36:14 +1300

    Add settings to git ignore

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://philanthropy:[REDACTED]@localhost/philanthropy';
+$db_url = 'mysql://philanthropy:root@localhost/philanthropy';
 $db_prefix = '';
 
 /**
